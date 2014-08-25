78ccebf95eb1ab94580aa9f14dda8fd0587cefa1
leoleong <renzhejiajia@qq.com>
2014-08-25 14:55:57 +0800

    Add 'Reorder Array'.

---
+++ src/offer/_14_ReorderArray.java
@@ -0,0 +1,40 @@
+package offer;
+
+import java.util.Arrays;
+
+public class _14_ReorderArray {
+
+	public static void main(String[] args) {
+
+		int[] array = new int[] { 4, 6, 8, 1, 3, 5 };
+		reorder(array);
+		System.out.println(Arrays.toString(array));
+	}
+
+	public static void reorder(int[] array) {
+
+		if (array == null || array.length <= 1) {
+			return;
+		}
+
+		partition(array);
+	}
+
+	private static void partition(int[] array) {
+
+		int i = -1;
+		for (int j = 0; j < array.length; j++) {
+			if (array[j] % 2 == 1) {
+				i++;
+				swap(array, i, j);
+			}
+		}
+	}
+
+	private static void swap(int[] array, int src, int dst) {
+
+		int temp = array[src];
+		array[src] = array[dst];
+		array[dst] = temp;
+	}
+}
