eb23ac1e318940e4c9ee848172dc8fdb4caa8f62
leoleong <renzhejiajia@qq.com>
2014-08-27 17:53:24 +0800

    Update 'Find in Partially Sorted Matrix'.

--- src/offer/_03_FindinPartiallySortedMatrix.java
+++ src/offer/_03_FindinPartiallySortedMatrix.java
@@ -2,30 +2,8 @@ package offer;
 
 import java.io.IOException;
 import java.io.StreamTokenizer;
-import java.util.Scanner;
 
 public class _03_FindinPartiallySortedMatrix {
-
-	// TLE
-	/*public static void main(String[] args) {
-
-		Scanner sc = new Scanner(System.in);
-		while (sc.hasNext()) {
-			int row = sc.nextInt();
-			int column = sc.nextInt();
-			int target = sc.nextInt();
-
-			int[][] array = new int[row][column];
-			for (int i = 0; i < row; i++) {
-				for (int j = 0; j < column; j++) {
-					array[i][j] = sc.nextInt();
-				}
-			}
-
-			boolean exist = search(array, target);
-			System.out.println(exist ? "Yes" : "No");
-		}
-	}*/
 
 	public static void main(String[] args) throws IOException {
 
