ce711fcdaa35c6a6de9aa1aec9d0e896d5e9156c
leoleong <renzhejiajia@qq.com>
2014-08-27 17:52:54 +0800

    Update 'Reverse List'.

--- src/offer/_16_ReverseList.java
+++ src/offer/_16_ReverseList.java
@@ -4,15 +4,15 @@ public class _16_ReverseList {
 
 	public static void main(String[] args) {
 
-		ListNode node1 = new ListNode(1);
-		ListNode node2 = new ListNode(2);
-		ListNode node3 = new ListNode(3);
-		ListNode node4 = new ListNode(4);
-		node1.next = node2;
-		node2.next = node3;
-		node3.next = node4;
+		int[] array = new int[] { 1, 2, 3, 4 };
+		ListNode dummy = new ListNode(-1);
+		ListNode node = dummy;
+		for (int i : array) {
+			node.next = new ListNode(i);
+			node = node.next;
+		}
 
-		ListNode head = reverse(node1);
+		ListNode head = reverse(dummy.next);
 
 		while (head != null) {
 			System.out.print(head.val + " ");
