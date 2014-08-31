34446003d587f5f4f05b387c18003f1712ca166c
leoleong <renzhejiajia@qq.com>
2014-08-31 09:17:08 +0800

    Update 'Add Two Numbers'.

--- src/offer/_47_AddTwoNumbers.java
+++ src/offer/_47_AddTwoNumbers.java
@@ -1,24 +1,20 @@
 package offer;
 
-import java.util.Scanner;
-
 public class _47_AddTwoNumbers {
 
 	public static void main(String[] args) {
 
-		Scanner sc = new Scanner(System.in);
-		while (sc.hasNext()) {
-			int op1 = sc.nextInt();
-			int op2 = sc.nextInt();
-			int result = addTwoNumbers(op1, op2);
-			System.out.println(result);
-		}
+		int op1 = -1;
+		int op2 = 1;
+		int result = addTwoNumbers(op1, op2);
+		System.out.println(result);
 	}
 
 	private static int addTwoNumbers(int op1, int op2) {
 
 		int value = 0;
 		int carry = 0;
+
 		do {
 			value = op1 ^ op2;
 			carry = (op1 & op2) << 1;
