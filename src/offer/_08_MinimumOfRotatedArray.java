27a3ef21132f9aecb34776b0559c2d6b12127536
leoleong <renzhejiajia@qq.com>
2014-09-09 11:52:16 +0800

    Update 'Minimum of Rotated Array'.

--- src/offer/_08_MinimumOfRotatedArray.java
+++ src/offer/_08_MinimumOfRotatedArray.java
@@ -1,36 +1,14 @@
 package offer;
 
 import java.io.IOException;
-import java.io.StreamTokenizer;
-import java.util.Scanner;
 
 public class _08_MinimumOfRotatedArray {
 
-	// Scanner : TLE
-	/*
-	 * public static void main(String[] args) {
-	 * 
-	 * Scanner sc = new Scanner(System.in); while (sc.hasNext()) { int T =
-	 * sc.nextInt(); int[] nums = new int[T]; for (int i = 0; i < T; i++) {
-	 * nums[i] = sc.nextInt(); }
-	 * 
-	 * int min = minimumOfRotatedArray(nums); System.out.println(min); } }
-	 */
-
 	public static void main(String[] args) throws IOException {
 
-		StreamTokenizer st = new StreamTokenizer(System.in);
-		while (st.nextToken() != StreamTokenizer.TT_EOF) {
-			int T = (int) st.nval;
-			int[] nums = new int[T];
-			for (int i = 0; i < T; i++) {
-				st.nextToken();
-				nums[i] = (int) st.nval;
-			}
-
-			int min = minimumOfRotatedArray(nums);
-			System.out.println(min);
-		}
+		int[] nums = new int[] { 1, 1, 1, 0, 1, 1 };
+		int min = minimumOfRotatedArray(nums);
+		System.out.println(min);
 	}
 
 	private static int minimumOfRotatedArray(int[] nums) {
@@ -45,14 +23,12 @@ public class _08_MinimumOfRotatedArray {
 				// couldn't judge order
 				start++;
 			} else if (nums[mid] > nums[start]) {
-				// left is sorted
-				if (nums[start] > nums[end]) {
+				if (nums[start] >= nums[end]) {
 					start = mid + 1;
 				} else {
-					end = mid - 1;
+					return nums[start];
 				}
 			} else {
-				// right is sorted
 				end = mid - 1;
 			}
 			min = min < nums[mid] ? min : nums[mid];
