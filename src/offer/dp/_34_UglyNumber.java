7955edd114743023d61bfec3e00968c1fe8a42db
leoleong <renzhejiajia@qq.com>
2014-09-12 16:50:44 +0800

    Update 'Ugly Number'.

--- src/offer/dp/_34_UglyNumber.java
+++ src/offer/dp/_34_UglyNumber.java
@@ -4,40 +4,44 @@ public class _34_UglyNumber {
 
 	public static void main(String[] args) {
 
-		int result = getUglyNumber(5);
+		int n = 5;
+		int result = getUglyNumber(n);
 		System.out.println(result);
 	}
 
-	public static int getUglyNumber(int index) {
+	public static int getUglyNumber(int n) {
 
-		int[] nums = new int[index + 1];
-		nums[0] = 1;
+		if (n == 0) {
+			return 0;
+		}
+		
+		int[] nums = new int[n + 1];
+		nums[1] = 1;
 
-		for (int i = 1; i < nums.length; i++) {
-			int current = Integer.MAX_VALUE;
-			for (int j = i - 1; j >= 0; j--) {
-				int temp5 = nums[j] * 5;
-				int temp3 = nums[j] * 3;
+		for (int i = 2; i < nums.length; i++) {
+			int temp = Integer.MAX_VALUE;
+			for (int j = i - 1; j > 0; j--) {
 				int temp2 = nums[j] * 2;
+				int temp3 = nums[j] * 3;
+				int temp5 = nums[j] * 5;
 
+				// pruning
 				if (temp5 < nums[i - 1]) {
 					break;
 				}
 
-				if (temp5 > nums[i - 1] && temp5 < current) {
-					current = temp5;
-				}
-				if (temp3 > nums[i - 1] && temp3 < current) {
-					current = temp3;
-				}
-				if (temp2 > nums[i - 1] && temp2 < current) {
-					current = temp2;
+				if (temp2 > nums[i - 1] && temp2 < temp) {
+					temp = temp2;
+				} else if (temp3 > nums[i - 1] && temp3 < temp) {
+					temp = temp3;
+				} else if (temp5 > nums[i - 1] && temp5 < temp) {
+					temp = temp5;
 				}
 
 			}
-			nums[i] = current;
+			nums[i] = temp;
 		}
 
-		return nums[index];
+		return nums[n];
 	}
 }
