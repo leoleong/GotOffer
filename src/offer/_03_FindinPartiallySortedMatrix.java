08b9481d6b062756b58df28cf60bb1016c565bf8
leoleong <renzhejiajia@qq.com>
2014-09-12 15:57:27 +0800

    Update 'Find in Partially Sorted Matrix'.

--- src/offer/_03_FindinPartiallySortedMatrix.java
+++ src/offer/_03_FindinPartiallySortedMatrix.java
@@ -1,37 +1,25 @@
 package offer;
 
-import java.io.IOException;
-import java.io.StreamTokenizer;
-
 public class _03_FindinPartiallySortedMatrix {
 
-	public static void main(String[] args) throws IOException {
+	public static void main(String[] args) {
 
-		StreamTokenizer st = new StreamTokenizer(System.in);
-		while (st.nextToken() != StreamTokenizer.TT_EOF) {
-			int row = (int) st.nval;
-			st.nextToken();
-			int column = (int) st.nval;
-			st.nextToken();
-			int target = (int) st.nval;
-
-			int[][] array = new int[row][column];
-			for (int i = 0; i < row; i++) {
-				for (int j = 0; j < column; j++) {
-					st.nextToken();
-					array[i][j] = (int) st.nval;
-				}
-			}
-
-			boolean exist = search(array, target);
-			System.out.println(exist ? "Yes" : "No");
-		}
+		int[][] array = new int[][] { { 1, 2, 8, 9 }, { 2, 4, 9, 12 },
+				{ 4, 7, 10, 13 }, { 6, 8, 11, 15 } };
+		int target = 7;
+		boolean exist = search(array, target);
+		System.out.println(exist);
 	}
 
 	private static boolean search(int[][] array, int target) {
 
+		if (array == null || array.length < 1) {
+			return false;
+		}
+
 		int i = 0;
 		int j = array[0].length - 1;
+
 		while (i < array.length && j >= 0) {
 			if (array[i][j] > target) {
 				j--;
