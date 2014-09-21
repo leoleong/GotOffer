7bf3a0efff6145d13a800e682e3ac88e0a9c27a2
leoleong <renzhejiajia@qq.com>
2014-09-21 16:02:11 +0800

    Update problem 'Number of 1'.

--- src/offer/_32_NumberOf1.java
+++ src/offer/_32_NumberOf1.java
@@ -1,30 +1,39 @@
 package offer;
 
-import java.io.IOException;
-import java.io.StreamTokenizer;
-
 public class _32_NumberOf1 {
 
-	public static void main(String[] args) throws IOException {
+	public static void main(String[] args) {
 
-		StreamTokenizer st = new StreamTokenizer(System.in);
-		while (st.nextToken() != StreamTokenizer.TT_EOF) {
-			int low = (int) st.nval;
-			st.nextToken();
-			int high = (int) st.nval;
-//			int result = numberOf1(low, high);
-//			System.out.println(result);
-		}
+		int n = 12;
+		int result = numberOf1(n);
+		System.out.println(result);
 	}
 
-//	private static int numberOf1(int low, int high) {
-//
-//		int tmp = high;
-//		int count = 0;
-//		while (tmp > 0) {
-//			count++;
-//			tmp /= 10;
-//		}
-//		
-//	}
+	private static int numberOf1(int n) {
+
+		if (n == 0) {
+			return 0;
+		}
+
+		int factor = 1;
+		int count = 0;
+		int lower, cur, higher;
+
+		while (n / factor != 0) {
+			lower = n - (n / factor) * factor;
+			cur = (n / factor) % 10;
+			higher = n / (factor * 10);
+
+			if (cur < 1) {
+				count += higher * factor;
+			} else if (cur == 1) {
+				count += higher * factor + lower + 1;
+			} else {
+				count += (higher + 1) * factor;
+			}
+			factor *= 10;
+		}
+
+		return count;
+	}
 }
